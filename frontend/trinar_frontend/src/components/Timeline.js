2434d0505808c6d8a8008c64a05758a415aa6b40
slvajunior <johnf.foto@gmail.com>
2025-01-15 12:32:01 -0300

    feat: PostField/Timeline/NavBar/SideBar/Home

--- frontend/trinar_frontend/src/components/Timeline.js
+++ frontend/trinar_frontend/src/components/Timeline.js
@@ -1,28 +1,60 @@
-//src/components/Timeline.js
-
+// src/components/Timeline.js
 import React from "react";
+import { FaThumbsUp, FaRetweet, FaComment } from "react-icons/fa"; // Importe os ícones
 import "./Timeline.css";
 
 const Timeline = () => {
   return (
     <div className="timeline-container">
       <div className="post">
-        <p>Eu não sei vocês mas sei lá eu </p>
+        <p>
+          Eu não sei vocês mas sei la eu.
+          <hr />
+          <ul className="lista-icones">
+            <li>
+              <FaThumbsUp /> {/* Ícone de like */}
+              <span>Like</span>
+            </li>
+            <li>
+              <FaRetweet /> {/* Ícone de repost */}
+              <span>Repost</span>
+            </li>
+            <li>
+              <FaComment /> {/* Ícone de comments */}
+              <span>Comments</span>
+            </li>
+          </ul>
+        </p>
       </div>
       <div className="post">
         <p>
-        isso de querer<br />
-        ser exatamente aquilo<br />
-        que a gente é<br />
-        ainda vai<br />
-        nos levar além<br /><br />
-        Paulo Leminski
-        <hr/>
-        </p>
-      </div>
-      <div>
-        <p>
-
+          isso de querer
+          <br />
+          ser exatamente aquilo
+          <br />
+          que a gente é
+          <br />
+          ainda vai
+          <br />
+          nos levar além
+          <br />
+          <br />
+          Paulo Leminski
+          <hr />
+          <ul className="lista-icones">
+            <li>
+              <FaThumbsUp /> {/* Ícone de like */}
+              <span>Like</span>
+            </li>
+            <li>
+              <FaRetweet /> {/* Ícone de repost */}
+              <span>Repost</span>
+            </li>
+            <li>
+              <FaComment /> {/* Ícone de comments */}
+              <span>Comments</span>
+            </li>
+          </ul>
         </p>
       </div>
     </div>
